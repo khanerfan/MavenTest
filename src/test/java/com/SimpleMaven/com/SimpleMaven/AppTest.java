0042fac47421a86131f5236e567684b24a2c53b8
khanerfan <khan.erfan88@gmail.com>
2017-02-01 02:11:59 +0530

    First commit

---
+++ src/test/java/com/SimpleMaven/com/SimpleMaven/AppTest.java
@@ -0,0 +1,34 @@
+package com.SimpleMaven.com.SimpleMaven;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.MarionetteDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+	WebDriver driver;
+
+	@BeforeTest
+	public void initiate() {
+		System.setProperty("webdriver.gecko.driver", "D:\\JavaWorkSpace\\SimpleMaven\\src\\ExecutableFiles\\geckodriver.exe");
+		driver = new MarionetteDriver();
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void run() {
+
+		driver.get("https://www.google.co.in");
+	}
+
+	@AfterTest
+	public void Close() {
+		// driver.close();
+	}
+}
